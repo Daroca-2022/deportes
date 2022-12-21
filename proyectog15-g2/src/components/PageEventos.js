14a2003db1717db8659f02b075e3a1c2d91bd6b4
FelipeMT22 <118862907+FelipeMT22@users.noreply.github.com>
2022-12-21 02:14:58 -0500

    Update PageEventos.js

--- proyectog15-g2/src/components/PageEventos.js
+++ proyectog15-g2/src/components/PageEventos.js
@@ -5,7 +5,7 @@ import {faEdit, faTrashAlt} from '@fortawesome/free-solid-svg-icons';
 import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
 import axios from "axios";
 
-const url = 'http://localhost:5000/eventos';
+const url = 'https://backendg15-02-production.up.railway.app/eventos';
 
 class PageEventos extends Component {
     state = {
